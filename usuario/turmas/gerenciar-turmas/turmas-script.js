9d330b7bb8756c06bb32ffafc3fe0b7ef41c1fbd
Art_Mainardi <iensenmainardi@gmail.com>
2026-06-12 21:42:41 -0300

    Adicionado ad funcções de 'listar' e 'buscar'

--- usuario/turmas/gerenciar-turmas/turmas-script.js
+++ usuario/turmas/gerenciar-turmas/turmas-script.js
@@ -1,3 +1,62 @@
+async function listarTurmas(){
+    const listaHTML = document.getElementById("lista-turmas");
+    listaHTML.innerHTML = "Carregando...";
+
+    const resposta = await fetch('http://localhost:8055/turmas', {
+        method: 'GET'
+    });
+    const turmas = await resposta.json();
+    listaHTML.innerHTML = "";
+
+    // Verificar se a listaHTML não está vazia:
+    if(turmas.length == 0){
+        listaHTML.innerHTML = "<li>Lista vazia: nenhuma turma encontrada.</li>";
+        return;
+    }
+
+    // Criando a listaHTML:
+    let listaGerada = "";
+    turmas.forEach(turma => {
+        listaGerada += `
+            <li>
+                <strong>| ID:</strong> ${turma.id_turma} | 
+                <strong> Nome:</strong> ${turma.nome_turma} |
+                <strong> Período:</strong> ${turma.periodo.periodo} |
+            </li>
+        `;
+    });
+    listaHTML.innerHTML = listaGerada;
+}
+
+// --------------------------------
+async function procurarTurmas(){
+    const feedback = document.getElementById("turma");
+    feedback.innerHTML = "Procurando...";
+
+    // Procurar aluno pelo ID:
+    const id = document.getElementById("id-turma").value;
+    const API = await fetch(`http://localhost:8055/turmas/${id}`, {
+        method: 'GET'
+    });
+    const procura = await API.json();
+    feedback.innerHTML = "";
+
+    // Verificar se a procura não está vazia:
+    if(procura.length == 0 || !API.ok){
+        feedback.innerHTML = "<li>Nenhuma turma com esse ID foi encontrado.</li>";
+        return;
+    }
+
+    // Mostrando o aluno pesquisado:
+    feedback.innerHTML = `
+        <li>
+            <strong>| ID:</strong> ${procura.id_turma} | 
+            <strong> Nome:</strong> ${procura.nome_turma} |
+            <strong> Período:</strong> ${procura.periodo.periodo} |
+        </li>
+    `;
+}
+
 // --------------------------------
 async function salvarProfessores(modificar){
     const mensagem = document.getElementById("feedback");
@@ -8,16 +67,18 @@ async function salvarProfessores(modificar){
     // Criando objeto da turma:
     const novaTurma = {
         nome_turma: document.getElementById("nome-turma").value,
-        periodo: document.getElementById("periodo").value
+        periodo: {
+            id_periodo: document.getElementById("periodo").value
+        }
     };
     if(modificar){
         novaTurma.id_turma = document.getElementById("id-alvo").value;
         metodoFetch = 'PUT';
-        modificarURL = `/${novaTurma.id_professor}`;
+        modificarURL = `/${novaTurma.id_turma}`;
     }
 
     // Enviando aluno para a API:
-    const API = await fetch(`http://localhost:8055/professor${modificarURL}`, {
+    const API = await fetch(`http://localhost:8055/turmas${modificarURL}`, {
         method: `${metodoFetch}`,
         headers: {
             'Content-Type': 'application/json' // Avisa o Spring que estamos enviando um JSON
