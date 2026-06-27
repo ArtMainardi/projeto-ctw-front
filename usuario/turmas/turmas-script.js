76f6f2a1033208371d9d1669ebec7ea63dbe5e90
Art_Mainardi <iensenmainardi@gmail.com>
2026-06-27 08:11:27 -0300

    Lista de turmas funcional com lógica de página de detalhes para cada item

--- usuario/turmas/turmas-script.js
+++ usuario/turmas/turmas-script.js
@@ -1,20 +1,37 @@
-window.addEventListener("load", () => {
-    async function carregarTurmas(){
-        const listaMI = document.getElementById("lista-mi");
+window.addEventListener("load", async function carregarTurmas(){
+    // Criar lista de turmas de acordo com o curso:
+    const listaMI = document.querySelector(".lista-mi");
 
-        const API = await fetch("http://localhost:8055/turmas", {
-            method: "GET"
-        });
-        const turmas = await API.json();
+    const API = await fetch("http://localhost:8055/turmas", {
+        method: "GET"
+    });
+    const turmas = await API.json();
 
-        if(turmas.length != 0){
-            let listaGeradaMI = "";
-            for(let cont = 0; cont < turmas.length; cont++){
-                if(turmas[cont].nome_turma.toLowerCase().includes("mi")){
-                    listaGeradaMI += `<li>${turmas.nome_turma}  |  <strong>Período:</strong> ${turmas.periodo}</li>`
-                }
+    if(turmas.length != 0){
+        let listaGeradaMI = "";
+        for(let cont = 0; cont < turmas.length; cont++){
+            console.log(turmas[cont].nome_turma)
+            if(turmas[cont].nome_turma.toLowerCase().includes("mi")){
+                listaGeradaMI += `
+                    <p class="item-lista" data-id="${cont}" style="cursor: pointer;">
+                        ${turmas[cont].nome_turma}  |  <strong>Período:</strong> ${turmas[cont].periodo.periodo}
+                    </p>
+                `;
             }
-            listaGeradaMI !== "" ? listaMI.innerHTML = listaGeradaMI : listaMI.innerHTML = "Nenhuma turma desse curso encontrada!";
+            console.log(listaGeradaMI)
         }
+        listaGeradaMI !== "" ? listaMI.innerHTML = listaGeradaMI : listaMI.innerHTML = "Nenhuma turma desse curso encontrada!";
+    } else {
+        document.querySelector('.lista-cursos').innerHTML = "Nenhuma turma salva!";
     }
+
+
+    // Listener para abrir as páginas de detalhes:
+    document.querySelectorAll(".item-lista").forEach((item) => {
+    console.log("SSSSS")
+    item.addEventListener("click", () => {
+        console.log("SSSS")
+        window.location.href = `./turmaDetalhes.html?id=${item.dataset.id}`;
+    });
+});
 });
